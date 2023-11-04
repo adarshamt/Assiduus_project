da0fdfd757068d4564e3fb0ae00338d9a50b2fb3
adarshamt <adarshamt@gmail.com>
2023-11-04 18:20:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import "./App.css";
 import Navbar from "./Componets/Navbar.jsx";
 import Sidebar from "./Componets/Sidebar.jsx";
 
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Box from "@mui/material/Box";
+
+
 function App() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <>
       <div className="Dash_main_container">
@@ -18,6 +32,51 @@ function App() {
             <div className="compnent_">
               <div className="compnent_nav">
                 <span>Checking account</span>
+                <Box sx={{width:'17rem'}}>
+                <FormControl sx={{ m: 1, minWidth: 120, }} size="small">
+                  <InputLabel id="demo-select-small-label" sx={{backgroundColor:'#FFFF'}} >Manage</InputLabel>
+                  <Select
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>Manage</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Manage</MenuItem>
+                    <MenuItem value={20}>Manage</MenuItem>
+                    <MenuItem value={30}>Manage</MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                  <InputLabel id="demo-select-small-label"sx={{backgroundColor:'#FFFF'}}>Month</InputLabel>
+                  <Select
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>January</MenuItem>
+                    <MenuItem value={20}>February</MenuItem>
+                    <MenuItem value={30}>March</MenuItem>
+                    <MenuItem value={30}>April</MenuItem>
+                    <MenuItem value={30}>May</MenuItem>
+                    <MenuItem value={30}>June</MenuItem>
+                    <MenuItem value={30}>July</MenuItem>
+                    <MenuItem value={30}>August</MenuItem>
+                    <MenuItem value={30}>September</MenuItem>
+                    <MenuItem value={30}>October</MenuItem>
+                    <MenuItem value={30}>November</MenuItem>
+                    <MenuItem value={30}>December</MenuItem>
+                  </Select>
+                </FormControl>
+                </Box>
               </div>
             </div>
             <div className="compnent_">
